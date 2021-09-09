3e7a5c3f8b19647f9053fc35687de6f5519b786c
TMGA <tomasgajda261@gmail.com>
2021-09-09 21:33:33 +0200

    pridana pravdepodobnost

--- FrequencyAnalysis.py
+++ FrequencyAnalysis.py
@@ -17,8 +17,8 @@ bpmb bpr xjhhjcwko wi bpr sujsru msshwvmbwjk mkd
 wkbrusurbmbwjk w jxxru yt bprjuwri wk bpr pjsr bpmb bpr
 riirkvr jx jqwkmcmk qmumbr cwhh urymwk wkbmvb"""
 
-
-
+#######################################################################################################################################
+""" 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 freq = {} #slovnik
 letter_count = 0
@@ -31,23 +31,66 @@ for c in alphabet: #kazdemu pismenu v abecede priradi nulu
     #.
     #.
 
-
 for c in cipher: #pro vsechny znaky v sifre 
     if c in freq: #jestli je ve freq priradi jedna
         freq[c] += 1 #projizdi se retezec bere se kazde pismenko a inkrementuje se mu hodnota o 1
         letter_count += 1
 
-
 for c in freq:
     freq[c] = round(freq[c]/letter_count,4) #pocita se vyskyt v procente a zaokrouhluje se hodnota na 4 desetina mista
 
 
-new_line_count = 0
-
-#
-for c in freq: # 
+for c in freq: 
     print(c, ':', freq[c],' ', end='') #parametr end na konec vypisu vklada mezere
     if new_line_count % 3 == 2: #pocitame od 0 pak 1 a pak 2 a hele zbytek 2 hodim tam enter
         print()
     new_line_count += 1
 
+"""
+class Attack:
+    def __init__(self):
+        self.alphabet = "abcdefghijklmnopqrstuvwxyz"
+        self.freq = {}
+        self.freq_eng = {'a': 0.0817, 'b': 0.0150, 'c': 0.0278, 'd': 0.0425, 'e': 0.1270, 'f': 0.0223, 'g': 0.0202, 'h': 0.0609, 'i': 0.0697, 'j': 0.0015, 'k': 0.0077, 'l': 0.0403, 'm': 0.0241, 'n': 0.0675, 'o': 0.0751, 'p': 0.0193, 'q': 0.0010, 'r': 0.0599, 's': 0.0633, 't': 0.0906, 'u': 0.0276, 'v': 0.0098, 'w': 0.0236, 'x': 0.0015, 'y': 0.0197, 'z': 0.0007}
+        self.mappings = {}
+
+
+
+    def calculate_freq(self, cipher):
+        for c in self.alphabet: #kazdemu pismenu v abecede priradi nulu 
+            self.freq[c] = 0 #vytvori list
+        
+        letter_count = 0
+        
+        for c in cipher: #pro vsechny znaky v sifre 
+            if c in self.freq: #jestli je ve freq priradi jedna
+                self.freq[c] += 1 #projizdi se retezec bere se kazde pismenko a inkrementuje se mu hodnota o 1
+                letter_count += 1
+
+        for c in self.freq:
+            self.freq[c] = round(self.freq[c]/letter_count,4) #pocita se vyskyt v procente a zaokrouhluje se hodnota na 4 desetina mista
+
+    def print_freq(self):
+        new_line_count = 0
+        for c in self.freq: 
+            print(c, ':', self.freq[c],' ', end='') #parametr end na konec vypisu vklada mezere
+            if new_line_count % 3 == 2: #pocitame od 0 pak 1 a pak 2 a hele zbytek 2 hodim tam enter
+                print()
+            new_line_count += 1
+
+    def calculate_matches(self):
+        for cipher_char in self.alphabet:  
+            map = {}
+            for plain_char in self.alphabet: #pro kazdy znak v abecede
+                map[plain_char] = round(abs(self.freq[cipher_char] - self.freq_eng[plain_char]), 4) #ulozim do slovniku a odectu pravdepodovnosti pro kazde pismeno
+            self.mappings[cipher_char] = sorted(map.items(), key=operator.itemgetter(1)) #srovnam ho od nejmensi po nejvetsi
+
+
+attack = Attack()
+attack.calculate_freq(cipher)
+attack.print_freq()
+attack.calculate_matches()
+
+print()
+for c in attack.mappings: #a vsechno vypisu
+    print(c, attack.mappings[c])
